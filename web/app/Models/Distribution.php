23d584422a6e8aa28211804292455a484ef9fe4c
Tudose Valentin <tudose.valentin21@gmail.com>
2020-12-31 14:14:27 +0200

    final touches

--- web/app/Models/Distribution.php
+++ web/app/Models/Distribution.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Distribution extends Model
 {
-    protected array $guarded = [];
+    protected $guarded = [];
 
     protected $casts = [
         'value' => 'array'
